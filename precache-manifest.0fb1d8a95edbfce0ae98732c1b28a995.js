d1a852b66ca6659fa113155c692f2582a193ccdc
hwisaac <54179672+hwisaac@users.noreply.github.com>
2022-12-09 07:50:56 +0900

    Updates

---
+++ precache-manifest.0fb1d8a95edbfce0ae98732c1b28a995.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a87fe7c85bfc2af34620",
+    "url": "/react-deploy-test/static/js/main.a87fe7c8.chunk.js"
+  },
+  {
+    "revision": "ca1cb434d895d00c18c8",
+    "url": "/react-deploy-test/static/js/1.ca1cb434.chunk.js"
+  },
+  {
+    "revision": "c98906c62e9b0564cc06",
+    "url": "/react-deploy-test/static/js/runtime~main.c98906c6.js"
+  },
+  {
+    "revision": "7ca800b1b29b78028649a14a9a191b69",
+    "url": "/react-deploy-test/index.html"
+  }
+];
